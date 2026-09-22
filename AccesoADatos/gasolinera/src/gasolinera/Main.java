d7eebaa1dc38507ab5739350fc5ba79a97b7fb2d
Alumno Mañana <Alumno Maana@LB-2DAM-01.iesluisbraille.es>
2026-09-22 09:18:05 +0200

    entrega parcial

--- AccesoADatos/gasolinera/src/gasolinera/Main.java
+++ AccesoADatos/gasolinera/src/gasolinera/Main.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Main  {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
     }
 }
